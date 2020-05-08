c3a7e8a9aa54d14d19b843888f7a4747671f10d6
Vovamzur <vovamazur3@gmail.com>
2020-05-09 01:36:44 +0300

    add booking models, migrations, seesders

---
+++ booking/src/data/models/order.js
@@ -0,0 +1,40 @@
+export default (orm, DataTypes) => {
+  const Order = orm.define(
+    'order',
+    {
+      clientID: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+      },
+      driverID: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.ENUM('pending', 'submited', 'started', 'finished', 'canceled')
+      },
+      from: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      to: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      date: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      price: {
+        allowNull: true,
+        type: DataTypes.FLOAT
+      },
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    },
+    {},
+  );
+
+  return Order;
+};
---
+++ booking/src/data/seeders/1315238165238-fill-db.js
@@ -0,0 +1,8 @@
+export default {
+  up: async (queryInterface, Sequelize) => {
+    try {
+    } catch (err) {
+
+    }
+  }
+}
